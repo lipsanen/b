8f93777a29d6963729478a28fc25a2d6d669b3f9
lipsanen <jukspaz@gmail.com>
2023-09-13 13:46:29 +0300

    Clean up the plugin implementation a bit

--- src/ob_plugin/plugin.cpp
+++ src/ob_plugin/plugin.cpp
@@ -6,22 +6,22 @@ class OBPlugin : public IServerPluginCallbacks
 {
     virtual bool Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory)  override;
     virtual void Unload( void ) override;
-    virtual void Pause( void ) override;
-    virtual void UnPause( void ) override;
+    virtual void Pause( void ) override {}
+    virtual void UnPause( void ) override {}
     virtual const char *GetPluginDescription( void ) override;
-    virtual void LevelInit( char const *pMapName ) override;
-    virtual void ServerActivate( edict_t *pEdictList, int edictCount, int clientMax ) override;
-    virtual void GameFrame( bool simulating ) override;
-    virtual void LevelShutdown( void ) override;
-    virtual void ClientActive( edict_t *pEntity ) override;
-    virtual void ClientDisconnect( edict_t *pEntity ) override;
-    virtual void ClientPutInServer( edict_t *pEntity, char const *playername ) override;
-    virtual void SetCommandClient( int index ) override;
-    virtual void ClientSettingsChanged( edict_t *pEdict ) override;
+    virtual void LevelInit( char const *pMapName ) override {}
+    virtual void ServerActivate( edict_t *pEdictList, int edictCount, int clientMax ) override {}
+    virtual void GameFrame( bool simulating ) override {}
+    virtual void LevelShutdown( void ) override {}
+    virtual void ClientActive( edict_t *pEntity ) override {}
+    virtual void ClientDisconnect( edict_t *pEntity ) override {}
+    virtual void ClientPutInServer( edict_t *pEntity, char const *playername ) override {}
+    virtual void SetCommandClient( int index ) override {}
+    virtual void ClientSettingsChanged( edict_t *pEdict ) override {}
     virtual PLUGIN_RESULT ClientConnect( bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen ) override;
     virtual PLUGIN_RESULT ClientCommand( edict_t *pEntity, const CCommand &args ) override;
     virtual PLUGIN_RESULT NetworkIDValidated( const char *pszUserName, const char *pszNetworkID ) override;
-    virtual void OnQueryCvarValueFinished( QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue ) override;
+    virtual void OnQueryCvarValueFinished( QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue ) override {}
 };
 
 static OBPlugin plugin;
@@ -54,16 +54,3 @@ PLUGIN_RESULT OBPlugin::NetworkIDValidated( const char *pszUserName, const char 
 {
     return PLUGIN_CONTINUE;
 }
-
-void OBPlugin::Pause( void ){}
-void OBPlugin::UnPause( void ){}
-void OBPlugin::LevelInit( char const *pMapName ){}
-void OBPlugin::ServerActivate( edict_t *pEdictList, int edictCount, int clientMax ){}
-void OBPlugin::GameFrame( bool simulating ){}
-void OBPlugin::LevelShutdown( void ){}
-void OBPlugin::ClientActive( edict_t *pEntity ){}
-void OBPlugin::ClientDisconnect( edict_t *pEntity ){}
-void OBPlugin::ClientPutInServer( edict_t *pEntity, char const *playername ){}
-void OBPlugin::SetCommandClient( int index ){}
-void OBPlugin::ClientSettingsChanged( edict_t *pEdict ){}
-void OBPlugin::OnQueryCvarValueFinished( QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue ){}
--- src/steampipe_plugin/plugin.cpp
+++ src/steampipe_plugin/plugin.cpp
@@ -6,24 +6,24 @@ class SteampipePlugin : public IServerPluginCallbacks
 {
     virtual bool Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory)  override;
     virtual void Unload( void ) override;
-    virtual void Pause( void ) override;
-    virtual void UnPause( void ) override;
+    virtual void Pause( void ) override {}
+    virtual void UnPause( void ) override {}
     virtual const char *GetPluginDescription( void ) override;
-    virtual void LevelInit( char const *pMapName ) override;
-    virtual void ServerActivate( edict_t *pEdictList, int edictCount, int clientMax ) override;
-    virtual void GameFrame( bool simulating ) override;
-    virtual void LevelShutdown( void ) override;
-    virtual void ClientActive( edict_t *pEntity ) override;
-    virtual void ClientDisconnect( edict_t *pEntity ) override;
-    virtual void ClientPutInServer( edict_t *pEntity, char const *playername ) override;
-    virtual void SetCommandClient( int index ) override;
-    virtual void ClientSettingsChanged( edict_t *pEdict ) override;
+    virtual void LevelInit( char const *pMapName ) override {}
+    virtual void ServerActivate( edict_t *pEdictList, int edictCount, int clientMax ) override {}
+    virtual void GameFrame( bool simulating ) override {}
+    virtual void LevelShutdown( void ) override {}
+    virtual void ClientActive( edict_t *pEntity ) override {}
+    virtual void ClientDisconnect( edict_t *pEntity ) override {}
+    virtual void ClientPutInServer( edict_t *pEntity, char const *playername ) override {}
+    virtual void SetCommandClient( int index ) override {}
+    virtual void ClientSettingsChanged( edict_t *pEdict ) override {}
     virtual PLUGIN_RESULT ClientConnect( bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen ) override;
     virtual PLUGIN_RESULT ClientCommand( edict_t *pEntity, const CCommand &args ) override;
     virtual PLUGIN_RESULT NetworkIDValidated( const char *pszUserName, const char *pszNetworkID ) override;
-    virtual void OnQueryCvarValueFinished( QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue ) override;
-    virtual void OnEdictAllocated( edict_t *edict ) {}
-    virtual void OnEdictFreed(const edict_t *edict ) {}	
+    virtual void OnQueryCvarValueFinished( QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue ) override {}
+    virtual void OnEdictAllocated( edict_t *edict ) override {}
+    virtual void OnEdictFreed(const edict_t *edict ) override {}	
 };
 
 static SteampipePlugin plugin;
@@ -56,16 +56,3 @@ PLUGIN_RESULT SteampipePlugin::NetworkIDValidated( const char *pszUserName, cons
 {
     return PLUGIN_CONTINUE;
 }
-
-void SteampipePlugin::Pause( void ){}
-void SteampipePlugin::UnPause( void ){}
-void SteampipePlugin::LevelInit( char const *pMapName ){}
-void SteampipePlugin::ServerActivate( edict_t *pEdictList, int edictCount, int clientMax ){}
-void SteampipePlugin::GameFrame( bool simulating ){}
-void SteampipePlugin::LevelShutdown( void ){}
-void SteampipePlugin::ClientActive( edict_t *pEntity ){}
-void SteampipePlugin::ClientDisconnect( edict_t *pEntity ){}
-void SteampipePlugin::ClientPutInServer( edict_t *pEntity, char const *playername ){}
-void SteampipePlugin::SetCommandClient( int index ){}
-void SteampipePlugin::ClientSettingsChanged( edict_t *pEdict ){}
-void SteampipePlugin::OnQueryCvarValueFinished( QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue ){}
